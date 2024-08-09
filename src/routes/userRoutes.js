73345956922df442d51441a718817debb87308c2
JENNY-257 <j.iribagiza2@gmail.com>
2024-08-09 19:57:38 +0200

    user reset password

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,27 @@
 import { Router } from 'express';
-import { registerUser, userLogin, verifyuserEmail } from '../controllers/authentication/userController.js';
-import { loginSchemaValidation, signupSchemaValidation } from '../validations/userValidation.js';
+import {
+registerUser, 
+sendEmailToUser, 
+userLogin, 
+userResetPassword, 
+verifyuserEmail 
+} 
+from '../controllers/authentication/userController.js';
+
+import { 
+emailSchemaValidation,
+loginSchemaValidation,
+signupSchemaValidation,
+resetPasswordValidation 
+}
+from '../validations/userValidation.js';
 
 const userRoute = Router();
 
 userRoute.post('/signup', signupSchemaValidation,registerUser);
 userRoute.get('/verify-email/:accessToken',verifyuserEmail);
 userRoute.post('/login', loginSchemaValidation,userLogin);
+userRoute.post('/send-email',emailSchemaValidation,sendEmailToUser);
+userRoute.post('/reset-password/:token',resetPasswordValidation,userResetPassword);
 
 export default userRoute;
--- src/validations/userValidation.js
+++ src/validations/userValidation.js
@@ -49,3 +49,42 @@ export const loginSchemaValidation = (req,res,next) => {
     }
     
 }
+
+const emailSchema = Joi.object().keys({
+  email:Joi.string().email().required(),
+})
+
+
+const validateEmail = validateForm(emailSchema);
+
+export const emailSchemaValidation = (req,res,next) => {
+    const { error } = validateEmail(req.body);
+    if (error) {
+      res.status(400).json({
+        status: 400,
+        error: error.details.map((detail) => detail.message.replace(/[^a-zA-Z0-9 ]/g, '')),
+      });
+    } else {
+      next();
+    } 
+}
+
+const resetSchema = Joi.object().keys({
+  newPassword:Joi.string().min(4).max(20).required(),
+  confirmPassword:Joi.string().min(4).max(20).required(),
+
+})
+
+const validateResetSchema = validateForm(resetSchema);
+
+export const resetPasswordValidation = (req,res,next) => {
+  const { error } = validateResetSchema(req.body);
+  if (error) {
+    res.status(400).json({
+      status: 400,
+      error: error.details.map((detail) => detail.message.replace(/[^a-zA-Z0-9 ]/g, '')),
+    });
+  } else {
+    next();
+  } 
+}
